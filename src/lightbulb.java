26944955974a9e61f0a20ec64dab83d011083ca5
strecksh <strecksh8883@uwec.edu>
2019-11-14 12:58:39 -0600

    Updated to create array of lightbulbs

--- src/lightbulb.java
+++ src/lightbulb.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class lightbulb {
 
 
     public static void main(String[] args){
 
+        Random rand = new Random();
         Scanner userIn = new Scanner(System.in);
         System.out.println("How many switches would you like?");
         int numSwitches = userIn.nextInt();
+        int[] lightBulbArray = new int[numSwitches];
+
+
+        int counter = 0;
+
+        while(counter != numSwitches){
+
+            lightBulbArray[counter] = rand.nextInt(2);
+            counter++;
+        }
+
+        for(int i = 0; i < lightBulbArray.length; i++){
+
+
+            System.out.println(lightBulbArray[i]);
+
+        }
 
 
     }

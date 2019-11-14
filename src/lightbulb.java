4c731518636740b95c0746a594bbb5dc4043b591
strecksh <strecksh8883@uwec.edu>
2019-11-13 22:27:27 -0600

    Initial commit

---
+++ src/lightbulb.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class lightbulb {
+
+
+    public static void main(String[] args){
+
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("How many switches would you like?");
+        int numSwitches = userIn.nextInt();
+
+
+    }
+
+
+
+
+
+
+
+
+
+}

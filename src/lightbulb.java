c9ecca0a2a47f27c51463481fa26d2d8834b4cf4
strecksh <strecksh8883@uwec.edu>
2019-11-14 14:56:06 -0600

    Started work on the algorithm

--- src/lightbulb.java
+++ src/lightbulb.java
@@ -5,38 +5,70 @@ import java.util.Random;
 public class lightbulb {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Random rand = new Random();
+        boolean lightSwitchState = false;
         Scanner userIn = new Scanner(System.in);
         System.out.println("How many switches would you like?");
         int numSwitches = userIn.nextInt();
         int[] lightBulbArray = new int[numSwitches];
 
-
         int counter = 0;
 
-        while(counter != numSwitches){
+        while (counter != numSwitches) {
 
             lightBulbArray[counter] = rand.nextInt(2);
             counter++;
         }
 
-       /* for(int i = 0; i < lightBulbArray.length; i++){
+//        for(int i = 0; i < lightBulbArray.length; i++){
+//
+//            System.out.println(lightBulbArray[i]);
+//
+//        }
 
-            System.out.println(lightBulbArray[i]);
+        if (bruteForce(lightBulbArray)) { //checks to see if the switches are all on without flipping a switch
+            lightSwitchState = true;
+        }
 
-        }*/
+        int firstSwitchVal = lightBulbArray[0]; //stores the value of the first switch for later use
+
+        for (int i = 0; i <= numSwitches; i++) {
+
+            lightBulbArray[i] = 1 - i;
+            if (bruteForce(lightBulbArray)) {
+                lightSwitchState = true;
+            }
+
+        }
+
+        for (int j = 1; j <= numSwitches; j++) {
+
+
+        }
 
 
     }
 
 
+    public static boolean bruteForce(int[] input) {
 
+        //Cover the edge cases that the input array is null or has one element.
+        if (input == null || input.length == 1)
+            return true; //Returning true for null is debatable, but I leave that to you.
 
+        int compare = input[0]; //Compare to the first element of the input array.
 
+        //Check from the second element through the end of the input array.
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] != compare)
+                return false;
+        }
 
+        return true;
 
+    }
 
 
 }

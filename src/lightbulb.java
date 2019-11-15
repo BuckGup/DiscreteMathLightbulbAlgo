d5147c1ba21ff001ddf4e586526c495c0d9f80b0
strecksh <strecksh8883@uwec.edu>
2019-11-14 22:34:36 -0600

    Debugging

--- src/lightbulb.java
+++ src/lightbulb.java
@@ -7,46 +7,60 @@ public class lightbulb {
 
     public static void main(String[] args) {
 
+
         Random rand = new Random();
         boolean lightSwitchState = false;
         Scanner userIn = new Scanner(System.in);
         System.out.println("How many switches would you like?");
         int numSwitches = userIn.nextInt();
         int[] lightBulbArray = new int[numSwitches];
+        int[] binaryArray = new int[numSwitches];
+        String binaryString = Integer.toBinaryString(numSwitches); //converts the num of switches to binary
+        char[] binaryChars = binaryString.toCharArray(); //converts String to char array for use in loop later
 
         int counter = 0;
 
         while (counter != numSwitches) {
-
             lightBulbArray[counter] = rand.nextInt(2);
             counter++;
         }
 
-//        for(int i = 0; i < lightBulbArray.length; i++){
-//
-//            System.out.println(lightBulbArray[i]);
-//
-//        }
 
-        if (isOne(lightBulbArray)){
-            lightSwitchState = true;
+        for(int i = 0; i < lightBulbArray.length; i++){
 
-            System.out.println("The light bulb is on");
-            return;
+            System.out.println(lightBulbArray[i]);
+
         }
 
-        for(int l = 0; l <= (2^numSwitches) - 1; l++){
 
-            System.out.println(Integer.toBinaryString(l));
+      /*  if (isOne(lightBulbArray)) {
+            lightSwitchState = true;
+            System.out.println("The light bulb is on");
+            return;
+        }*/
 
+        System.out.println("New line converted");
+        for (int j = 0; j < (binaryChars.length); j++) {
 
+            if(binaryChars[j] == 1) {
+                lightBulbArray[j] = 1 - lightBulbArray[j];
+
+                System.out.println(lightBulbArray[j]);
+
+                if(isOne(lightBulbArray)){
+                    lightSwitchState = true;
+                    System.out.println("The light bulb is on");
+                    return;
+                }
+
+            }
         }
 
 
         //take the binary number and go across each string looking for 1. If you find one take that index and change it of the original switch array
 
 
-        for (int i = 0; i < numSwitches; i++) {
+       /* for (int i = 0; i < numSwitches; i++) {
 
             lightBulbArray[i] = 1 - lightBulbArray[i];
             if (bruteForce(lightBulbArray)) {
@@ -56,7 +70,6 @@ public class lightbulb {
 
         }
 
-        returnOrginal(lightBulbArray); //returns array to original form
 
         for (int j = 1; j <= numSwitches; j++) {
             lightBulbArray[j] = 1 - lightBulbArray[j];
@@ -65,7 +78,7 @@ public class lightbulb {
                 return;
             }
 
-        }
+        }*/
 
 
     }
@@ -73,8 +86,8 @@ public class lightbulb {
     private static boolean isOne(int[] lightBulbArray) {
         boolean lightSwitchState;
         boolean isOne = true;
-        for(int i = 0; i < lightBulbArray.length; i++) {//checks to see if the switches are all on without flipping a switch
-            if(lightBulbArray[i] != 1){
+        for (int i = 0; i < lightBulbArray.length; i++) {//checks to see if the switches are all on without flipping a switch
+            if (lightBulbArray[i] != 1) {
                 isOne = false;
             }
         }
@@ -102,7 +115,7 @@ public class lightbulb {
 
     public static void returnOrginal(int[] input) {
 
-        for (int i = 0; i <= input.length; i++) {
+        for (int i = 0; i < input.length; i++) {
             input[i] = 1 - i;
         }
 
